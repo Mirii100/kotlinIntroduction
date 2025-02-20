b5c931ff28e2dafd5c46bf93ecaeba1d7418cb19
Alex <njugunamirii99@gmail.com>
2025-02-20 17:58:24 +0100

    message

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        println("i = $i")
+
+//this is how class is written
+open class Car{
+    class car()
+    val brand="mazda "
+    val model="vt"
+
+    fun choose(){
+    val year=2020
+
+    }
+
+}
+//this is how inheritance is done
+class carType: Car(){
+
+    fun myfunc(){
+        println("this is the function i am witting in kotlin ")
+
     }
 }
+//main function
+fun main() {
+
+    println("here is the introduction  ")
+
+
+    add(10,20)
+    var firstObject= Car() //calling for that obects
+    println(firstObject) // printing objects in the screen
+
+    var second=carType()
+    println("second object is written as$second")
+    second.myfunc()
+}
+//add function
+fun add(x:Int,y:Int){
+    println(x+y)
+    //data type
+    //int,string ,booleam,float
+
+}
+
+
